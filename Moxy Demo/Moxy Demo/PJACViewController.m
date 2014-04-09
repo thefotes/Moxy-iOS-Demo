9f17a57322c826ef979e0c7ae69d6787006b3e57
Andrew Crites <acrites@mobiquityinc.com>
2014-04-09 18:48:58 -0400

    Log data from server

--- Moxy Demo/Moxy Demo/PJACViewController.m
+++ Moxy Demo/Moxy Demo/PJACViewController.m
@@ -15,6 +15,21 @@
 @implementation PJACViewController
 - (void)buttonPressed:(UIButton *)sender
 {
-    NSLog(@"Button: %d", sender.tag);
+    NSURL *baseUrl = [NSURL URLWithString:@"http://192.168.1.92:3000"];
+    
+    NSURL *requestUrl = [NSURL URLWithString:@"" relativeToURL:baseUrl];
+    
+    NSMutableURLRequest *apiRequest = [NSMutableURLRequest requestWithURL:requestUrl];
+    
+    apiRequest.HTTPMethod = @"GET";
+    
+    [NSURLConnection sendAsynchronousRequest:apiRequest
+                                       queue:[NSOperationQueue mainQueue]
+                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
+                               
+                               NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
+                               NSLog(@"%@", json);
+                           }];
 }
 @end
