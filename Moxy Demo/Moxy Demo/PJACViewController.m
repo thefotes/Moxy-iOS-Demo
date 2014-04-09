24c5c65ecb730f79eeaa0034de527a1256b24e2f
Andrew Crites <acrites@mobiquityinc.com>
2014-04-09 19:14:50 -0400

    Adding table view for response display

--- Moxy Demo/Moxy Demo/PJACViewController.m
+++ Moxy Demo/Moxy Demo/PJACViewController.m
@@ -8,13 +8,22 @@
 
 #import "PJACViewController.h"
 
-@interface PJACViewController ()
-@property (weak, nonatomic) IBOutlet UILabel *responseLabel;
-
+@interface PJACViewController () <UITableViewDataSource,UITableViewDelegate>
 - (IBAction)buttonPressed:(UIButton *)sender;
+@property (strong, nonatomic) NSMutableArray *responses;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @end
 
 @implementation PJACViewController
+
+- (NSMutableArray *)responses
+{
+    if (!_responses) {
+        _responses = [NSMutableArray new];
+    }
+    
+    return _responses;
+}
 - (void)buttonPressed:(UIButton *)sender
 {
     NSURL *baseUrl = [NSURL URLWithString:@"http://192.168.1.92:3000"];
@@ -30,8 +39,25 @@
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *connectionError) {
                                
                                NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+                               
+                               [self.responses addObject:json];
+                               
+                               [self.tableView reloadData];
 
-                               self.responseLabel.text = json;
                            }];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    
+    cell.textLabel.text = [self.responses objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.responses.count;
 }
 @end
